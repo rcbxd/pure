94ba86c03fb88d82597b67c235a99df33bfcb764
rcbxd <50280805+rcbxd@users.noreply.github.com>
2023-05-14 00:44:17 -0500

    (prompt) parse full directory name

--- functions/_pure_parse_directory.fish
+++ functions/_pure_parse_directory.fish
@@ -1,29 +1,14 @@
-function prompt_pwd
-  set -q argv[1]
-  and switch $argv[1]
-    case -h --help
-      __fish_print_help prompt_pwd
-      return 0
-  end
-
-  set -q fish_prompt_pwd_dir_length
-  or set -l fish_prompt_pwd_dir_length 1
-  set ttmp $PWD
-  set ttmp (string replace -r '^'$HOME'($|/)' '~$1' $PWD)
-  set -l tmp (basename $ttmp)
-
-  if [ $fish_prompt_pwd_dir_length -eq 0 ];
-    echo $tmp
-  else
-    string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' $tmp
-  end
-end
-
 function _pure_parse_directory \
     --description "Replace '$HOME' with '~'" \
     --argument-names max_path_length
 
-    set folder (prompt_pwd)
+    set --local folder (string replace $HOME '~' $PWD)
 
+    if test -n "$max_path_length";
+        if test (string length $folder) -gt $max_path_length;
+            # If path exceeds maximum symbol limit, use default fish path formating function
+            set folder (prompt_pwd)
+        end
+    end
     echo $folder
 end
